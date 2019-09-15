4e8adcf5c1641b3d78893a94c7f470305746678c
Belle P <bellepx3@gmail.com>
2019-09-15 21:42:45 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -3,9 +3,9 @@ class Application
    def call(env)
     resp = Rack::Response.new
     
-    if Time.new 
-    noon = Time.parse "12:00 pm"
-#=> 2015-11-23 22:27:00 -0800
-current_time = Time.now 
-#=> 2015-11-23 19:38:06 -0800
-current_time < close_time
+  noon = Time.parse "12:00 pm"
+  current_time = Time.now 
+  
+  if current_time < noon
+    resp.write 
+   
